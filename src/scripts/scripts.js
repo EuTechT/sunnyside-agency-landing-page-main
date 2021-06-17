9bea5f957a2f75485738296f63963b49c31dc1a3
André Filipe <eutech.backup@gmail.com>
2021-06-17 18:12:37 -0300

    Completed project

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,6 +1,17 @@
 const btnMenu = window.document.querySelector('.btn-menu');
 const nav = window.document.querySelector('.navigation');
+const header1 = window.document.querySelector('.header-1');
 
 btnMenu.addEventListener('click', () => {
     nav.classList.toggle('navigation--active');
+    header1.classList.toggle('header-1--active');
 });
+
+window.onresize = closeMenuMobile;
+
+function closeMenuMobile() {
+    if(window.innerWidth > 1024) {
+        nav.classList.remove('navigation--active');
+        header1.classList.remove('header-1--active');
+    }
+}
